e5d0f747d3d667c003a2472aa7016883544bc9fc
Stoyan Stoyanov <sss17@mail.com>
2014-10-08 18:36:48 +0300

    Fixed getting user by id... added functionlity for admin promotion

--- Client/app/controllers/userDetailsCtrl.js
+++ Client/app/controllers/userDetailsCtrl.js
@@ -1,7 +1,14 @@
 app.controller('UserDetailsCtrl', function($scope,$routeParams,auth, notifier, UsersResource) {
 
+   //not working... don't know why
+    var currentUser= UsersResource.get({id:$routeParams.id.toString()}, function () {
+       $scope.currentUser = currentUser;
+    });
+
 
     $scope.update = function(user) {
+        user.roles=[user.roles[0]];
+
         auth.updateOther(user,$routeParams.id).then(function(success) {
             notifier.success('User successfully updated!');
         }, function (error) {
