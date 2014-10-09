a25e12b5c32e7727efaa58610bcdbe0f71b8ed89
Stoyan Stoyanov <sss17@mail.com>
2014-10-09 13:49:35 +0300

    Added paging and sorting of jokes...

--- Client/app/controllers/jokesCtrl.js
+++ Client/app/controllers/jokesCtrl.js
@@ -1,4 +1,27 @@
 app.controller('JokesCtrl', function($scope, JokesResource) {
-    $scope.jokes = JokesResource.query();
-    console.log($scope.jokes);
+    $scope.request = {
+        page: 0
+    };
+    $scope.jokes = JokesResource.query($scope.request);
+    $scope.previousPage = function() {
+        if ($scope.request.page > 0) {
+            $scope.request.page--;
+            $scope.filter($scope.request);
+        }
+    }
+
+    $scope.nextPage = function() {
+        $scope.request.page++;
+        $scope.filter($scope.request);
+    }
+    $scope.filter = function(request) {
+        console.log(request);
+        JokesResource.query(request)
+            .$promise
+            .then(function(jokes) {
+                $scope.jokes = jokes;
+            });
+    };
+
+
 });
