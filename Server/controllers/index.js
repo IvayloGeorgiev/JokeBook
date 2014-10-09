52e942c59ce02b7f83bc4fc4f0eb2e0443365c98
susankelvin <susankelvin@users.noreply.github.com>
2014-10-09 15:45:36 +0300

    Likes implemented

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -3,9 +3,11 @@
 var usersController = require('../controllers/usersController');
 var jokesController = require('../controllers/jokesController');
 var commentsController = require('../controllers/commentsController');
+var likesController = require('../controllers/likesController');
 
 module.exports = {
     users: usersController,
     jokes: jokesController,
-    comments: commentsController
+    comments: commentsController,
+    likes: likesController
 };
