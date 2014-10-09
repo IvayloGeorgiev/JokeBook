51af1189db7a3c473d41359ae71f81b1d5acaa74
Stoyan Stoyanov <sss17@mail.com>
2014-10-09 22:37:46 +0300

    Gave up... heroku not working... :@

--- Server/config/config.js
+++ Server/config/config.js
@@ -4,8 +4,7 @@ var rootPath = path.normalize(__dirname + '/../../')
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://admin:123456@ds033380.mongolab.com:33380/jokebook',
-        //db: 'mongodb://localhost:27017/Jokebook',
+        db: 'mongodb://localhost:27017/Jokebook',
         port: process.env.PORT || 3030
     },
     production: {
